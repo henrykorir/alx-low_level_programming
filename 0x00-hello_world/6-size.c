d32fca53f3d55826c3bc2e9c1dc2c9245e8000cd
henrykorir <longkhery@gmail.com>
2023-02-16 12:14:33 -0800

    using sizeof operator

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("size of a char: %d bytes(s)\n", (int)sizeof(char));
+	printf("size of an int: %d bytes(s)\n", (int)sizeof(int));
+	printf("size of a long int: %d byte(s)\n", (int)sizeof(long int));
+	printf("size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
+	printf("sizeof a float: %d byte(s)\n",(int)sizeof(float));
+
+	return(0);
+}
