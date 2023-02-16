b993b782a67a78b7d5a0b752e2c5e55c089237e4
henrykorir <longkhery@gmail.com>
2023-02-16 12:21:57 -0800

    using sizeof operator

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+/*
+ * finding out the 
+ * size of data types
+ */
 int main(void)
 {
 	printf("size of a char: %d bytes(s)\n", (int)sizeof(char));
 	printf("size of an int: %d bytes(s)\n", (int)sizeof(int));
 	printf("size of a long int: %d byte(s)\n", (int)sizeof(long int));
 	printf("size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
-	printf("sizeof a float: %d byte(s)\n",(int)sizeof(float));
+	printf("sizeof a float: %d byte(s)\n", (int)sizeof(float));
 
-	return(0);
+	return (0);
 }
