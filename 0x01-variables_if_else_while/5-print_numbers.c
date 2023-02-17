dbe35be30c41d06feeb232afb78664249819dce8
henrykorir <longkhery@gmail.com>
2023-02-17 08:31:42 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	unsigned int index = 0;
+
+	for(index = 0; index <= 9; index++)
+		printf("%d", index);
+	printf("\n");
+	return (0);
+}
