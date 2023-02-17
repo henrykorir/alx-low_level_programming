4e75b45083db3f445eaf724b041e49587c8ad4b1
henrykorir <longkhery@gmail.com>
2023-02-17 12:43:49 -0800

    combinatorics

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,17 +9,17 @@ int main(void)
 	unsigned int i = 3;
 	unsigned int value = 1;
 
-	while (i <= 10)
+	while (i <= 11)
 	{
 		while ((value % 10) != 0)
 		{
-			if (value <= 9)
-				putchar('0');
-			else
-				putchar((int)('0' + (value / 10)));
+			putchar((int)('0' + (value / 10)));
 			putchar((int)('0' + (value % 10)));
-			if (i <= 10)
+			if ((value + 10) != 99)
+			{
+				putchar(',');
 				putchar(' ');
+			}
 			value++;
 		}
 		value--;
