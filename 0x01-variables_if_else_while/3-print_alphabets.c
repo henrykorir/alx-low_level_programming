a0c5f8d9bb8bb51805451a1d3cbedb37660dfc02
henrykorir <longkhery@gmail.com>
2023-02-17 08:06:21 -0800

    generate both case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	unsigned int index = 0;
+
+	while (index <= 25)
+	{
+		putchar((int)('a' + index));
+		index++;
+	}
+	index = 0;
+	
+	while (index <= 25)
+	{
+		putchar((int)('A' + index));
+		index++;
+	}
+	putchar('\n');
+
+	return (0);
+}
