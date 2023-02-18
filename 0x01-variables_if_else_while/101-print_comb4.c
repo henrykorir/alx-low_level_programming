205268483b78d4d8ab100c0b276063a8411c7f2e
henrykorir <longkhery@gmail.com>
2023-02-17 23:35:28 -0800

    Using loop and if,if..else to do combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -23,7 +23,7 @@ int main(void)
 						putchar((int)('0' + i));
 						putchar((int)('0' + j));
 						putchar((int)('0' + k));
-						if (i < 7)
+					        if (i < 7)
 						{
 							putchar(',');
 							putchar(' ');
