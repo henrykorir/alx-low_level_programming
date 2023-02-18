d575f1b41676f9bd11dfdc8d74657b82e6878658
henrykorir <longkhery@gmail.com>
2023-02-17 23:17:46 -0800

    Using loop and if,if..else to do combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+       	unsigned int i = 0;
+        unsigned int j = 0;
+        unsigned int k = 0;
+		
+		while (i <= 9)
+		{
+			while (j <= 9)
+			{
+				while (k <= 9)
+				{
+					if (i != j && i != k && j != k)
+					{
+						if (i < j && j < k)
+						{
+							putchar((int)('0' + i));
+							putchar((int)('0' + j));
+							putchar((int)('0' + k));
+							if (i < 7)
+							{
+								putchar(',');
+								putchar(' ');
+							}
+						}
+					}
+					k++;
+				}
+				k = 0;
+				j++;
+			}
+			j=0;
+			i++;
+		}
+        putchar('\n');
+		
+        return (0);
+}
