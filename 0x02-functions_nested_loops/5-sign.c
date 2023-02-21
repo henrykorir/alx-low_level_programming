65e3e011b04940d3b3a5e2a72fcbc1f104dd7359
henrykorir <longkhery@gmail.com>
2023-02-21 12:53:37 -0800

    printing the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - Prints characteristics of a
+ * character
+ *
+ * @n: number to be tested
+ *
+ * Return: 1 and + for n > 0
+ * 0 for n == 0
+ * -1 and - for n < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return (-1);
+}
