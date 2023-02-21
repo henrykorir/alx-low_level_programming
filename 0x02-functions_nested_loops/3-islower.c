038dadbb377ddf2379cf30cf8705e0b20c1c58e4
henrykorir <longkhery@gmail.com>
2023-02-21 12:10:04 -0800

    finding if alphabet is a lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - Checks if a character is
+ * lowercase alphabet
+ *
+ * @c: character to be tested
+ *
+ * Return: 0 for false or 1 otherwise
+ */
+int _islower(int c)
+{
+	if ((c>= ((int)('a'))) && (c <= ((int)('z'))))
+		return (1);
+	return (0);
+}
