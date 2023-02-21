66b643aa310497faa77883e656808366a7bf49e0
henrykorir <longkhery@gmail.com>
2023-02-21 11:45:38 -0800

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times
+ * in lowercase
+ *
+ * Return: Nothing
+ */
+void print_alphabet_x10(void)
+{
+	unsigned int i = 0;
+
+	for (i = 1; i <= 10; i++)
+		print_alphabet();
+}
