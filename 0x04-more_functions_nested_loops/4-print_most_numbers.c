272f5040c5c846bd8d49502ce2cbac677f5c468b
henrykorir <longkhery@gmail.com>
2023-02-23 13:06:52 -0800

    skip numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - Print number 0 - 1 and 3 - 9
+ *
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+	char c = '0';
+
+	for (; c <= '9'; c++)
+	{
+		if (c != '2' && c != '4')
+			_putchar(c);
+	}
+}
