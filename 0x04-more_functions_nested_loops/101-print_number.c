405ddef41ed4e54faf28e5d2800d685d0888a926
henrykorir <longkhery@gmail.com>
2023-02-24 14:16:08 -0800

    printing number by digits

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,17 +1,27 @@
+#include <limits.h>
 #include "main.h"
+
 /**
 * print_number - Print number by digits
 * @n: The number
 */
 void print_number(int n)
 {
-	int divisor = 10, number = n, sign = 0;
+	int divisor = 10, sign = 0;
+	unsigned int number;
 
-	if (number < 0)
+	if (n == INT_MIN)
+	{
+		number = (unsigned int)n;
+		sign = 1;
+	}
+	else if (n < 0 && n != INT_MIN)
 	{
 		sign = 1;
-		number *= -1;
+		number = -1 * n;
 	}
+	else
+		number = n;
 	if (sign == 1)
 		_putchar('-');
 	if (number <= 9)
@@ -20,7 +30,7 @@ void print_number(int n)
 	}
 	else
 	{
-		while ((number / divisor) > 10)
+		while ((number / divisor) >= 10)
 		{
 			divisor *= 10;
 		}
