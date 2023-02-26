f82993ed91a2689d73905402d1f73675372221a1
henrykorir <longkhery@gmail.com>
2023-02-26 01:40:26 -0800

    print maximum prime factor of 612852475143

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,23 +5,24 @@
  */
 int main(void)
 {
-	long int n = 612852475143, prime = 2081, max = prime, i;
+	unsigned long long int n = 612852475143, prime = 2, max = prime, i;
 
-	do {
-		while (n % prime == 0)
-		{
-			if (prime > max)
+	while(prime <= n)
+	{
+		while ((n % prime) == 0){
+			printf("%lld, %lld\n", n, prime);
+			if(prime > max)
 				max = prime;
 			n /= prime;
 		}
-		prime = prime + 2;
-		for (i = 2; i < prime && prime <= n; i += 1)
+		prime++;
+		for (i = 2; ((i * i) <= prime) && prime <= n; i++)
 		{
-			if ((prime % i) == 0)
-				prime += 2;
+			if (prime % i == 0)
+				prime++;
 		}
-	} while (prime <= n);
-	printf("%ld\n", max);
+	}
+	printf("%lld\n", max);
 
 	return (0);
 }
