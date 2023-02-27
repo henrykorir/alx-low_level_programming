95401f681ad965718cda76445b4e3150fc767983
henrykorir <longkhery@gmail.com>
2023-02-27 13:23:43 -0800

    print elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_array - print array
+ * elements in csv
+ *
+ * @a: array
+ * @n: size
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d", i);
+		if (i < (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
