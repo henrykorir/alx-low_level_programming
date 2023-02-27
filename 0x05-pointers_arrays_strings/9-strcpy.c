a29a878158e18b027fe537e17c3d7486bb982d04
henrykorir <longkhery@gmail.com>
2023-02-27 13:35:59 -0800

    custom strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcpy - copy string from
+ * source to destination
+ *
+ * @src: source
+ * @dest: destination
+ *
+ * Return: pointer  to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	while (*src != '\0' || *dest != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = '\0';
+
+	return (dest);
+}
