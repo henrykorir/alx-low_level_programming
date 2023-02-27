aa5a4fbbea6863fa527e6af4a274c7c783ad624a
henrykorir <longkhery@gmail.com>
2023-02-27 04:47:57 -0800

    chamge value in pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - changes the value to 98
+ * @n: pointer to number
+ * Description: assigns a new value to
+ * memory address
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
