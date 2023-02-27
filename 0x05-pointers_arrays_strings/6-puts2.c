97b306a32a529d4585cb007525fd036ca2e6768d
henrykorir <longkhery@gmail.com>
2023-02-27 12:48:37 -0800

    print even indexed characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,14 +5,12 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int length = 0, i = 0;
 
-	while ((i % 2 != 0 && str[i] != '\0') ||
-		(i % 2 == 0 && str[i + 1] != '\0'))
-	{
-		_putchar(*str);
-		str = str + 2;
-		i++;
-	}
+	while (str[length] != '\0')
+		length++;
+	for (i = 0; i < length && str[i] != '\0'; i += 2)
+		_putchar(str[i]);
 	_putchar('\n');
 }
+
