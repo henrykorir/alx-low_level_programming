e729e335bef6353be6eafb632f1f423ebbe72308
henrykorir <longkhery@gmail.com>
2023-02-27 17:57:44 -0800

    crackme password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,7 +7,20 @@
  */
 int main(void)
 {
-	srand(time(NULL));
-	
-	return (0);
+	char str[54];
+	unsigned int size;
+	char chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int key;
+
+	for (size = 1; size <= 5; size++)
+	{
+		--size;
+		for (size_t n = 0; n < size; n++)
+		{
+			key = rand() % ((int)(52 - 1));
+			str[n] = chars[key];
+		}
+		str[size] = '\0';
+		printf("%s\n", str);
+	}
 }
