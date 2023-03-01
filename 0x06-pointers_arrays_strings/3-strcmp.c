cfe9cdce210e9a28521f085c1ae8a44f61d00b5f
henrykorir <longkhery@gmail.com>
2023-03-01 06:35:02 -0800

    srcmp customized

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcmp - compare 2 strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0 for same, -1 for s1 < s2
+ * 1 for s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{       
+        int i = 0, j = 0, k = 0;
+
+	while (s1[i] == s2[j] && (s1[i] != '\0'	|| s2[i] != '\0'))
+		i++;
+
+	return (s1[i] - s2[i]);
+}
