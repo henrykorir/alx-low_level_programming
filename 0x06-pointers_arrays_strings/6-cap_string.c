1aa8ed4c8d7d335432735c6738020f3e083d9c1d
henrykorir <longkhery@gmail.com>
2023-03-01 11:20:55 -0800

    capitalize first letter of substring

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0' && ((sizeof(s) / sizeof(char)) > 0))
 	{
 		if ((s[i - 1] == ' ' || s[i - 1] == '\n' ||
 			s[i - 1] == '\v' || s[i - 1] == '\t' ||
