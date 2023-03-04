93f61c5dbd22369a0d830d1ddab93f3225c44a70
henrykorir <longkhery@gmail.com>
2023-03-04 00:42:25 -0800

    big integers addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,25 +12,24 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a = 0, b = 0, c = 0, sum_ab = 0;
-	int k1 = 0, k2 = 0, n = 0;
+	int i = 0, a = 0, b = 0, c = 0, sum_ab = 0;
+	int k = size_r, k1 = 0, k2 = 0, n = 0;
 
+	for (; i < size_r; i++)
+		r[i] = '0';
 	while (n1[k1] != '\0')
 		k1++;
 	while (n2[k2] != '\0')
 		k2++;
-	n = k1 >= k2 ? (k1 + 2) : (k2 + 2);
-	if (n <= size_r && size_r > 0)
+	n = k1 >= k2 ? k1 : k2;
+	if ((n + 1) <= k && k > 0)
 	{
-		n--;
-		r[n] = '\0';
-		n--;
-		while (n >= 0)
+		r[--k] = '\0';
+		k--;
+		while ((n--) >= 0)
 		{
-			if (k1 >= 0)
-				k1--;
-			if (k2 >= 0)
-				k2--;
+			k1--;
+			k2--;
 			if (k1 < 0)
 				a = 0;
 			if (k2 < 0)
@@ -42,11 +41,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 				a = n1[k1] - '0';
 				b = n2[k2] - '0';
 			}
-			sum_ab = a + b + c;
-			r[n] = '0' + sum_ab % 10;
-			c = sum_ab / 10;
-			n--;
+			if (k == 0 && (a + b + c) > 9)
+				return (0);
+			r[k--] = '0' + ((a + b + c) % 10);
+			c = (a + b + c) / 10;
 		}
+		i = 0;
+		while ((r[i++] = r[++k]) != '\0')
+			;
 	}
 	else
 		return (0);
