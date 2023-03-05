9fe391aa26ced7e2d1547cf176a79e83b3e3dde1
henrykorir <longkhery@gmail.com>
2023-03-05 03:37:04 -0800

    Print buffers in HEX format

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,35 +13,39 @@ void print_buffer(char *b, int size)
 	if (size > 0)
 	{
 		printf("%08x: ", 0);
-		for (i = 0; i < size; i++)
+		while (j < size)
 		{
-			if (i % 10 == 0 && i >= 10)
+			if (i < size)
 			{
-				for (; j < i; j++)
+				if (i % 10 == 0 && i >= 10)
 				{
-					if ((b[j] >= ' ') && (b[j] <= '~'))
-						printf("%c", b[j]);
-					else
-						printf(".");
+					for (; j < i; j++)
+						((b[j] >= ' ') && (b[j] <= '~')) ?
+					printf("%c", b[j]) : printf(".");
+					printf("\n%08x: ", i);
+					k = 0;
 				}
-				printf("\n%08x: ", i);
+				if (b[i] <= 16)
+					(b[i] == 0) ? printf("00") : printf("0%x", b[i]);
+				else
+					printf("%x", b[i]);
+				if (i % 2 != 0 && i >= 1)
+				{
+					putchar(' ');
+					k++;
+				}
+				k += 2;
+				i++;
 			}
-			if (b[i] <= 16)
-				(b[i] == 0) ? printf("00") : printf("0%x", b[i]);
-			else
-				printf("%x", b[i]);
-			if (i % 2 != 0 && i >= 1)
-				printf(" ");
-		}
-		k = ((10 - (i - j)) * 2) + ((i - j) / 2);
-		while (--k > 0)
-			printf(" ");
-		for (; j < i; j++)
-		{
-			if ((b[j] >= ' ') && (b[j] <= '~'))
-				printf("%c", b[j]);
-			else
-				printf(".");
+			if (i == size)
+			{
+				k = (24 - k) + 1;
+				for (; k > 0; k--)
+					putchar(' ');
+				for (; j < i; j++)
+					((b[j] >= ' ') && (b[j] <= '~')) ?
+				printf("%c", b[j]) : printf(".");
+			}
 		}
 		printf("\n");
 	}
