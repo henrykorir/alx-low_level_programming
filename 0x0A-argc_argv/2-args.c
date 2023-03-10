a6ba25eb1bbb0510bd62c13db9b8b05ae481ce9e
henrykorir <longkhery@gmail.com>
2023-03-10 11:04:29 -0800

    print all commands per line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: commands
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i < argc; i++)
+		printf("%s\n",argv[i]);
+
+	return (0);
+}
