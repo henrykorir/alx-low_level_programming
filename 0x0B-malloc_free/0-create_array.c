61513cf85fe3f0ca505bf35d19130cc3641498ce
henrykorir <longkhery@gmail.com>
2023-03-13 07:15:24 -0700

    added array of characters using malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	cptr = (char *)malloc(size * sizeof(char));
-	if (size == 0 || ptr == NULL)
+	if (size == 0 || cptr == NULL)
 		return (NULL);
 	for (; i < size; i++)
 	{
