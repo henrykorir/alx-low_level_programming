a4e0662cf1cbc7b8e33477a1f7dfa8bd5b30528b
henrykorir <longkhery@gmail.com>
2023-03-14 12:50:50 -0700

    split string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,38 +1,114 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * strtow - splits a string into words.
- * @str: string to be split
- * Return: a pointer to an array of strings (words)
- * or NULL otherwise
+ * check_for_space - checks if a character is a whitespaces
+ * @str: string to be checked
+ * @pos: character position in the string
+ * Return: 1 if it is a space, 0 otherwise
  */
-char **strtow(char *str)
+int check_for_space(char *str, int pos)
 {
-	int i = 0, j = 0, k = 0, m = 0, n = 0;
-	char **words;
+	int is_space =
+		str[pos] == ' ' ||
+		str[pos] == '\t' ||
+		str[pos] == '\n' ||
+		str[pos] == '\v' ||
+		str[pos] == '\f' ||
+		str[pos] == '\r';
 
-	if (str == NULL || str[0] == '\0')
-		return (NULL);
-	while (str[k] != '\0')
-		k++;
-	k++;
-	while (str[i] != '\0')
+	return (is_space);
+}
+/**
+ * skip_spaces - skips unnecessary whitespaces
+ * @str: string to skip spaces
+ * @pos: starting position
+ * Return: legal position, -1 otherwise
+ */
+int skip_spaces(char *str, int pos)
+{
+	if (check_for_space(str, pos) == 0)
+		return (pos);
+	while ((str + pos) != NULL && str[pos] != '\0')
 	{
-		if (str[i] == ' ')
-			m++;
-		i++;
+		if ((str + pos + 1) != NULL &&
+			str[pos + 1] != '\0' &&
+			check_for_space(str, pos + 1) == 0
+		)
+			return (pos + 1);
+		pos++;
 	}
-	i = 0;
-	while (str[i] != '\0')
+	if ((str + pos) == NULL || str[pos] == '\0')
+		return (-1);
+
+	return (pos);
+}
+/**
+ * get_height - calculate the height of the m x n array
+ * from a string.
+ * @str: the string to determine a m x n array height
+ * Return: height
+ */
+int get_height(char *str)
+{
+	int i = 0, m = 0;
+
+	while (check_for_space(str, i) == 1 || str[i] != '\0')
 	{
-		j++;
-		if (str[i] == ' ')
-			if (j > n)
-				n = j;
-		i++;
+		i = skip_spaces(str, i);
+		if (i >= 0)
+		{
+			m++;
+			i++;
+			while (str[i] != '\0' && check_for_space(str, i) == 0)
+				i++;
+		}
+	}
+	m++;
+
+	return (m);
+}
+/**
+ * get_width - calculate the width of the m x n array
+ * from a string.
+ * @str: the string to determine a m x n array width
+ * Return: width
+ */
+int get_width(char *str)
+{
+	int i = 0, j = 0, n = 0;
+
+	while (check_for_space(str, i) == 1 || str[i] != '\0')
+	{
+		i = skip_spaces(str, i);
+		if (i >= 0)
+		{
+			j++;
+			i++;
+			while (str[i] != '\0' && check_for_space(str, i) == 0)
+			{
+				i++;
+				j++;
+			}
+		}
+		if (j > n)
+			n = j;
+		j = 0;
 	}
 	n++;
-	words = malloc(m * sizeof(char*));
+
+	return (n);
+}
+/**
+ * allocate_memory - allocate m x n memory
+ * @m: the height
+ * @n: the width
+ * Return: 2d array memory or NULL otherwise
+ */
+char **allocate_memory(int m, int n)
+{
+	int i = 0, j = 0;
+	char **words = malloc(m * sizeof(char *));
+
 	if (words == NULL)
 		return (NULL);
 	for (i = 0; i < m; i++)
@@ -46,7 +122,57 @@ char **strtow(char *str)
 			return (NULL);
 		}
 	}
-	i = 0;
-while (str[i] != '\0')
+	return (words);
+}
+/**
+ * split_string - splits string into words
+ * @words: the memory to hold words
+ * @str: string to be split.
+ * @m: height of the memory.
+ * @n: the width of the memory.
+ * Return: 2d array of words
+ */
+char **split_string(char **words, char *str, int m, int n)
 {
+	int i = 0, j = 0, k = 0;
 
+	while (check_for_space(str, i) == 1 || str[i] != '\0')
+	{
+		i = skip_spaces(str, i);
+		if (i >= 0)
+		{
+			while (str[i] != '\0' && check_for_space(str, i) == 0)
+			{
+				words[j][k] = str[i];
+				i++;
+				k++;
+			}
+			words[j][k] = '\0';
+		}
+		j++;
+		k = 0;
+	}
+	words[j] = NULL;
+
+	return (words);
+}
+/**
+ * strtow - splits a string into words.
+ * @str: string to be split
+ * Return: a pointer to an array of strings (words)
+ * or NULL otherwise
+ */
+char **strtow(char *str)
+{
+	int m = 0, n = 0;
+	char **words;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	m = get_height(str);
+	n = get_width(str);
+	words = allocate_memory(m, n);
+	if (words == NULL)
+		return (NULL);
+	return (split_string(words, str, m, n));
+}
