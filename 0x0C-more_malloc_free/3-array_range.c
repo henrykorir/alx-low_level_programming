e7387c5b7f39fa74f574a04648658d8d5421735f
henrykorir <longkhery@gmail.com>
2023-03-16 13:43:54 -0700

    dynamic array of integers in a range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - create a dynamic array and assign
+ * values from min to max
+ * @min: lower bound value
+ * @max: highest bound value in the range
+ * Return: array of integers from min to max
+ */
+int *array_range(int min, int max)
+{
+	int i = 0, j = 0;
+	unsigned long  int size = 0;
+	int *array;
+
+	if (min > max)
+		return (NULL);
+	if (min >= 0 && max >= 0)
+		size = (max - min) + 1;
+	else if (min < 0 && max >= 0)
+		size = (min * -1) + 1 + max;
+	else
+		size = ((max * -1) - (min * -1)) + 1;
+	array = malloc(size * sizeof(int));
+	if (array == NULL)
+		return (NULL);
+	for (j = min; j<= max; j++)
+	{
+		array[i] = j;
+		i++;
+	}
+
+	return (array);
+}
