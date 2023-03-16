196698952f2e20758b4bd837ad44d5515f732431
henrykorir <longkhery@gmail.com>
2023-03-16 12:55:52 -0700

    custom calloc()

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - allocate and initialize  memory to 0.
+ * @nmemb: number of bytes of memory to allocate
+ * @size: byte size
+ * Return: the new memory pointer, NULL otherwise.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *mem;
+
+	mem = malloc(nmemb * size);
+	if (mem == NULL)
+		return (NULL);
+	for (; i < size; i++)
+		mem[i] = '0';
+
+	return ((void *)mem);
+}
