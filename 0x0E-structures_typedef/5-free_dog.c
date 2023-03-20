1bdbcf8a2afff04c635e5621d1cb04fdfe4cb46b
henrykorir <longkhery@gmail.com>
2023-03-20 13:25:55 -0700

    free struct memdory

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,6 +8,14 @@
 void free_dog(dog_t *d)
 {
 	if (d != NULL)
+	{
+		if (d->name != NULL)
+			free(d->name);
+		d->name = NULL;
+		if (d->owner != NULL)
+			free(d->owner);
+		d->owner = NULL;
 		free(d);
+	}
 	d = NULL;
 }
