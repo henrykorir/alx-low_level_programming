906cf0d09b908746a91d5660fa9defb9553aa09d
henrykorir <longkhery@gmail.com>
2023-03-20 11:49:30 -0700

    added code to initialize a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * init_dog - initializes dog structure
+ * @d: structure
+ * @name: struct first field
+ * @age: struct second field
+ * @owner: struct third field
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
