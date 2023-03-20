f20d3e761b2822a12a25328a17b52b8705127865
henrykorir <longkhery@gmail.com>
2023-03-20 13:09:12 -0700

    add new record creator

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a record of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to the new struct
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog_ptr = NULL;
+
+	new_dog_ptr = malloc(sizeof(dog_t));
+	if (new_dog_ptr == NULL)
+		return (NULL);
+	new_dog_ptr->name = name;
+	new_dog_ptr->age = age;
+	new_dog_ptr->owner = owner;
+
+	return (new_dog_ptr);
+}
