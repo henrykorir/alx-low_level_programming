1f6cb3115aaef09f73676f7e0264d6ede7c267c7
henrykorir <longkhery@gmail.com>
2023-03-20 13:39:30 -0700

    add new struct with data

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,15 +11,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog_ptr = NULL;
+	int i = 0;
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 	new_dog_ptr = malloc(sizeof(dog_t));
 	if (new_dog_ptr == NULL)
 		return (NULL);
-	new_dog_ptr->name = name;
+	while (name[i] != '\0')
+		i++;
+	if (i == 0)
+		return (NULL);
+	new_dog_ptr->name = malloc(i * sizeof(char));
+	if (new_dog_ptr->name == NULL)
+		return (NULL);
+	i = 0;
+	while ((new_dog_ptr->name[i] = name[i]) != '\0')
+		i++;
+	i = 0;
+	while (owner[i] != '\0')
+		i++;
+	if (i == 0)
+		return (NULL);
+	new_dog_ptr->owner = malloc(i * sizeof(char));
+	if (new_dog_ptr->owner == NULL)
+		return (NULL);
+	i = 0;
+	while ((new_dog_ptr->owner[i] = owner[i]) != '\0')
+		i++;
 	new_dog_ptr->age = age;
-	new_dog_ptr->owner = owner;
 
 	return (new_dog_ptr);
 }
