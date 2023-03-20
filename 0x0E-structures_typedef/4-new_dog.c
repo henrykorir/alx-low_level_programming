f93eba062c6fc063d8e1fd1fc81e745ff0a2ddb1
henrykorir <longkhery@gmail.com>
2023-03-20 13:59:17 -0700

    added new struct code

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,35 @@
 #include "dog.h"
 
 /**
+ * add_string - copies a string into a pointer
+ * @s: string to be added
+ * Return: pointer to the new string
+ */
+char *add_string(char *s)
+{
+	int i = 0;
+	char *str;
+
+	while (s[i] != '\0')
+		i++;
+	if (i == 0)
+		str = NULL;
+	else
+	{
+		str = malloc(i * sizeof(char));
+		if (str == NULL)
+			str = NULL;
+		else
+		{
+			i = 0;
+			while ((str[i] = s[i]) != '\0')
+				i++;
+		}
+	}
+
+	return (str);
+}
+/**
  * new_dog - creates a record of a dog
  * @name: name of the dog
  * @age: age of the dog
@@ -13,32 +42,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog_ptr = NULL;
 	int i = 0;
 
-	if (name == NULL || age < 0 || owner == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
 	new_dog_ptr = malloc(sizeof(dog_t));
 	if (new_dog_ptr == NULL)
 		return (NULL);
 	while (name[i] != '\0')
 		i++;
-	if (i == 0)
-		return (NULL);
-	new_dog_ptr->name = malloc(i * sizeof(char));
-	if (new_dog_ptr->name == NULL)
-		return (NULL);
-	i = 0;
-	while ((new_dog_ptr->name[i] = name[i]) != '\0')
-		i++;
-	i = 0;
-	while (owner[i] != '\0')
-		i++;
-	if (i == 0)
-		return (NULL);
-	new_dog_ptr->owner = malloc(i * sizeof(char));
-	if (new_dog_ptr->owner == NULL)
-		return (NULL);
-	i = 0;
-	while ((new_dog_ptr->owner[i] = owner[i]) != '\0')
-		i++;
+	new_dog_ptr->name = add_string(name);
+	new_dog_ptr->owner = add_string(owner);
 	new_dog_ptr->age = age;
 
 	return (new_dog_ptr);
