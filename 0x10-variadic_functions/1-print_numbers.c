186f00b981e95b0ebe944d1defa3c8b2b9bf667f
henrykorir <longkhery@gmail.com>
2023-03-23 13:15:49 -0700

    printing values of variadic arguments

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_numbers - print variadic argument values
+ * @separator: values spearator
+ * @n: expected number of arguments
+ * @...: variadic arguments
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int i = 0;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator != NULL && i < (n - 1))
+			printf("%s", separator);
+	}
+}
