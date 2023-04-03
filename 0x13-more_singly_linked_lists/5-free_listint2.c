dc3905af6dbbb8a5c540d90895d78e6114977cf8
henrykorir <longkhery@gmail.com>
2023-04-03 11:56:11 -0700

    added a list erasing function thant sets the head pointer to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - erases a list and set head pointer to NULL
+ * @head: pointer to the starting node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *del;
+
+	while (*head != NULL)
+	{
+		del = *head;
+		*head = (*head)->next;
+		free(del);
+		del = NULL;
+	}
+	head = NULL;
+}
