d2eca62c7b43efd65f74a78b6c56da51e7baf9b4
henrykorir <longkhery@gmail.com>
2023-04-04 09:27:44 -0700

    added a function to insert a node at a position in the list if possible

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,13 +13,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *curr, *prev;
 
 	new_node = malloc(sizeof(listint_t *));
-	if (new_node == NULL || head == NULL)
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
 	if (idx == 0 && (head == NULL || *head == NULL))
 	{
-		head = &new_node;
+		*head = new_node;
 		return (*head);
 	}
 	curr = *head;
