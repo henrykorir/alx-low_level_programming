951e28c15fd1dd91c970208f9e58605c87502a41
henrykorir <longkhery@gmail.com>
2023-04-05 09:04:11 -0700

    added a function to insert a node at a position in the list if possible

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,32 +9,32 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *copy = *head;
-	unsigned int node;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	unsigned int pos = 0;
+	listint_t *new_node, *curr, *prev;
+	new_node = malloc(sizeof(listint_t *));
+	if (new_node == NULL)
 		return (NULL);
-
-	new->n = n;
-
-	if (idx == 0)
+	new_node->n = n;
+	new_node->next = NULL;
+	curr = *head;
+	prev = *head;
+	while (pos < idx && curr != NULL)
 	{
-		new->next = copy;
-		*head = new;
-		return (new);
+		prev = curr;
+		curr = curr->next;
+		pos++;
 	}
-
-	for (node = 0; node < (idx - 1); node++)
+	if (pos == idx)
 	{
-		if (copy == NULL || copy->next == NULL)
-			return (NULL);
-
-		copy = copy->next;
+		if (curr != NULL)
+		{
+			new_node->next = curr;
+			prev->next = new_node;
+		}
+		else
+			prev->next = new_node;
+		return (new_node);
 	}
-
-	new->next = copy->next;
-	copy->next = new;
-
-	return (new);
+	free(new_node);
+	return (NULL);
 }
