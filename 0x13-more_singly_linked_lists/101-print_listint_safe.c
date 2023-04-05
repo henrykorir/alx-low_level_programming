f2cbdf4cef0b100dab99c637f32dd833255b0c0c
henrykorir <longkhery@gmail.com>
2023-04-05 13:17:12 -0700

    added a function to print listint_t safely to avoid infinite loops

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint_safe - safely prints listint_t linked list
+ * @head: address to the first node
+ * Return: number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t n = 0;
+	listint_t *current = (listint_t *)head;
+
+	if (head == NULL)
+		exit(98);
+	for (; current != NULL; current = current->next)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		n++;
+		if (current->next >= current)
+		{
+			printf("->[%p] 98\n", (void *)head);
+			exit(98);
+		}
+	}
+
+	return (n);
+}
