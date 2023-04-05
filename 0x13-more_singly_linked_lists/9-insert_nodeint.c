c662aa8d3339686c3bd4f922c7c824f4f14c8667
henrykorir <longkhery@gmail.com>
2023-04-05 03:39:30 -0700

    added a function to insert a node at a position in the list if possible

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,26 +9,26 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-        unsigned int pos = 0;
-        listint_t *new_node, *curr = NULL, *prev = NULL;
+	unsigned int pos = 0;
+	listint_t *new_node, *curr = NULL, *prev = NULL;
 
-        new_node = malloc(sizeof(listint_t *));
-        if (new_node == NULL)
-                return (NULL);
-        new_node->n = n;
-        new_node->next = NULL;
-        if (head == NULL && idx == 0)
-                return ((*head = new_node));
-        curr = *head;
-        prev = *head;
-        if ((curr + idx) == NULL && ((curr + (idx - 1)) == NULL))
-                return (NULL);
-        while (curr != NULL)
-        {
-                if (pos == idx)
-                {
-                        new_node->next = prev->next;
-                        prev->next = new_node;
+	new_node = malloc(sizeof(listint_t *));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	if (head == NULL && idx == 0)
+		return ((*head = new_node));
+	curr = *head;
+	prev = *head;
+	if ((curr + idx) == NULL && ((curr + (idx - 1)) == NULL))
+		return (NULL);
+	while (curr != NULL)
+	{
+		if (pos == idx)
+		{
+			new_node->next = prev->next;
+			prev->next = new_node;
 			return (new_node);
 		}
 		prev = curr;
