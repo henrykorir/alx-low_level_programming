9d5212b5a194d2dbaf15b4eb47e77018ed504ca6
henrykorir <longkhery@gmail.com>
2023-04-05 22:13:09 -0700

    initialized bit manipulation project folder

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * binary_to_uint -  converts a binary number to an unsigned int.
+ * @b: string of 0s and 1s.
+ * Return: unsigned integer
+ */
+unsigned int binary_to_uint(const char *b);
