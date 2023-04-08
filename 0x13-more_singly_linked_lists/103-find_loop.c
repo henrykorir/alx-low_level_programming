31a4e5ba84726d0cd4119a9842e8cf296f066755
henrykorir <longkhery@gmail.com>
2023-04-08 13:05:30 -0700

    added a function to find the begining of a loop in the listint_t list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ * @head: pointer to the first node
+ * Return:  The address of the node where the loop starts,
+ * or NULL if there is no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	size_t n = 0;
+	int direction;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+	if (head == head->next)
+		return (head);
+	direction = (head->next - head) < 0 ? -1 : 1;
+	while (head != NULL)
+	{
+		if (n > 0 && head->next != NULL)
+		{
+			if (!(((head->next - head) > 0 && direction > 0) ||
+			((head->next - head) < 0 && direction < 0)))
+			{
+				n++;
+				return(head);
+			}
+		}
+		head = head->next;
+		n++;
+	}
+
+	return (NULL);
+}
