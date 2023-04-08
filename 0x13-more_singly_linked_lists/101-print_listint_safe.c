f31d8483f2a1a66b5299cd06175224a5cf7e6e39
henrykorir <longkhery@gmail.com>
2023-04-08 02:50:08 -0700

    added code to safely print linkint_t list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,89 +1,37 @@
-#include "lists.h"
+#include <string.h>
 #include <stdio.h>
-
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
-
+#include <stdlib.h>
+#include "lists.h"
 /**
- * looped_listint_len - Counts the number of unique nodes
- *                      in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- *
- * Return: If the list is not looped - 0.
- *         Otherwise - the number of unique nodes in the list.
- */
-size_t looped_listint_len(const listint_t *head)
-{
-	const listint_t *tortoise, *hare;
-	size_t nodes = 1;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	tortoise = head->next;
-	hare = (head->next)->next;
-
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
-
-			return (nodes);
-		}
-
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
-	}
-
-	return (0);
-}
-
-/**
- * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
- *
- * Return: The number of nodes in the list.
+ * print_listint_safe - safely prints listint_t linked list
+ * @head: address to the first node
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes, index = 0;
+	listint_t *kipchoge = (listint_t *)head;
+	size_t steps = 0;
+	long int index = 0;
+	int visited[10000];
 
-	nodes = looped_listint_len(head);
-
-	if (nodes == 0)
+	if (visited == NULL)
+		exit(98);
+	memset(visited, 0, (10000 * sizeof(int)));
+	for (; kipchoge != NULL; kipchoge = kipchoge->next)
 	{
-		for (; head != NULL; nodes++)
+		if ((void *)kipchoge < (void *)head)
+			index = head - kipchoge;
+		else
+			index = kipchoge - head;
+		if (visited[index] == 1)
 		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
+			printf("-> [%p] %d\n", (void *)kipchoge, kipchoge->n);
+			return (steps);
 		}
+		printf("[%p] %d\n", (void *)kipchoge, kipchoge->n);
+		visited[index] = 1;
+		steps++;
 	}
 
-	else
-	{
-		for (index = 0; index < nodes; index++)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-
-		printf("-> [%p] %d\n", (void *)head, head->n);
-	}
-
-	return (nodes);
+	return (steps);
 }
-
