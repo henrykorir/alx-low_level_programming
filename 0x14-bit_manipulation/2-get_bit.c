17e6986b1dc631a3afcf5f149e2176304c540f94
henrykorir <longkhery@gmail.com>
2023-04-10 09:15:48 -0700

    added a function to get the bit at and index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,5 +8,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	return ((n & 1 << index) >> index);
+	if (index > ((sizeof(unsigned long int) * 8) - 1))
+		return (-1);
+	return ((n & (1 << index)) >> index);
 }
